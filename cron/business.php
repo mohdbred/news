efe711a0e6b531e8d1bdc89d5f2bfd5524627e2c
Mohd Belal <link.belal@gmail.com>
2018-04-01 20:52:40 +0530

    minor

--- cron/business.php
+++ cron/business.php
@@ -45,7 +45,7 @@ if ($err) {
 
 
                 $t = json_encode($value);
-                $test = serialize($t);
+                $test = serialize(str_replace("'", "\'", $t));
 
                 $sql = "INSERT INTO business (data) VALUES ('" . $test . "')";
                 if ($conn->query($sql) === TRUE) {
