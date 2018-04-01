f852391059ccf744a3ba691a8bbeaabb406e6bb0
Mohd Belal <link.belal@gmail.com>
2018-04-01 20:58:54 +0530

    minor

--- cron/arts.php
+++ cron/arts.php
@@ -46,7 +46,7 @@ if ($err) {
                 
 
                 $t = json_encode($value);
-                $test = serialize(str_replace("'", "\'", $t));
+                $test = (str_replace("'", "\'", $t));
 
                 $sql = "INSERT INTO arts (data) VALUES ('" . $test . "')";
                 if ($conn->query($sql) === TRUE) {
