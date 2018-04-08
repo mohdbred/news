8fc0c2190ebf57487072350afb83dc933371c405
Mohd Belal <link.belal@gmail.com>
2018-04-08 07:03:39 +0530

    minor belal

--- belal/contact.php
+++ belal/contact.php
@@ -11,20 +11,27 @@ $sender_message = stripslashes($_POST["message"]);
 $capcha = stripslashes($_POST["capcha"]);
 
 
-$url = 'https://www.google.com/recaptcha/api/siteverify';
-$data = array(
-    'secret' => '6Lf5s1EUAAAAAK1bTBO33JzzSBCducXvxIk-GEHB',
-    'response' => $capcha
-);
-$options = array(
-    'http' => array(
-        'method' => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context = stream_context_create($options);
-$verify = file_get_contents($url, false, $context);
-$captcha_success = json_decode($verify);
+//$url = 'https://www.google.com/recaptcha/api/siteverify';
+//$data = array(
+//    'secret' => '6Lf5s1EUAAAAAK1bTBO33JzzSBCducXvxIk-GEHB',
+//    'response' => $capcha
+//);
+//$options = array(
+//    'http' => array(
+//        'method' => 'POST',
+//        'content' => http_build_query($data)
+//    )
+//);
+//$context = stream_context_create($options);
+//$verify = file_get_contents($url, false, $context);
+//$captcha_success = json_decode($verify);
+
+
+
+$verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret=6Lf5s1EUAAAAAK1bTBO33JzzSBCducXvxIk-GEHB&response='.$$capcha);
+        $responseData = json_decode($verifyResponse);
+        
+        print_r($responseData);exit;
 if ($captcha_success->success == false) {
     echo "<p>You are a bot! Go away!</p>";
 } else if ($captcha_success->success == true) {
