0ed7506747d8b16c2fb8399efc6e82cc8c36214f
abdurraheem <mohdraheem06@gmail.com>
2018-04-18 22:10:16 +0530

    contact

--- mohammad/blog.php
+++ mohammad/blog.php
@@ -1,16 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "myblog";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
 
 $name = $_POST['name'];
 $email = $_POST['email'];
@@ -26,17 +14,25 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 }
 
 
-$sql = "INSERT INTO users (name, email,message,createdAt)
-VALUES ('$name', '$email', '$msg',CURRENT_TIMESTAMP)";
+    $to = 'mohdraheem06@gmail.com';
+    $subject = 'Mohammad AbdurRaheem Portfolio';
+    $message = 'Following person contacted you from your website <br> <b>Name :</b>' . $name . '<br><b>Email : </b>' . $email . '<br><b>Subject :</b>Some One Contacted You From your Portfolio Site<br><b>Message</b> : ' . $msg;
+    $header = 'MIME-Version: 1.0' . "\r\n";
+    $header .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $header .= "From: mohammad@newspulses.com \r\n" .
+            "Reply-To: mohammad@newspulses.com \r\n" .
+            "X-Mailer: PHP/" . phpversion();
 
 
-if ($conn->query($sql) === TRUE) {
-	$status['success'] = 'Thanks!. I will be in touch with you shortly.';
+  $retval = mail($to,$subject,$message,$header);
+
+ if( $retval == true ) {
+           $status['success'] = 'Thanks!. I will be in touch with you shortly.';
 	echo json_encode($status); return;
-} else {
-   $status['error'] = 'whoops! Looks like something wrong';
+         }else {
+            $status['error'] = 'whoops! Looks like something wrong';
 	echo json_encode($status); return;
-}
+         }
 
-$conn->close();
 ?>
